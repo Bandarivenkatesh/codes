c61c323125f2588ea41ad5345edf089bab1ecfc4
venkatesh <mr.bandarivenkatesh@gmail.com>
2018-02-17 16:12:29 +0530

    master file

---
+++ bitwise.c
@@ -0,0 +1,42 @@
+//gcc -D0R bitwise.c   if you want AND ==> -DAND  
+#include <stdio.h>
+
+int main()
+{
+
+#ifdef OR
+   int x=4;
+   int bitpo=1;
+   x |= 1<<bitpo;
+   printf("x = %d\n",x);
+#endif
+
+#ifdef AND
+   int x=4;
+   int bitpo=2;
+   x &= ~(1<<bitpo);
+   printf("x is 4 bits 100 after clear %d bit the out put is ==%d\n",bitpo,x);
+#endif
+
+#ifdef TOG
+   int x=4;
+   int bitpo=2;
+   x ^= 1<<bitpo;
+   printf("100 clear the 2bit is %d\n",x);
+#endif  
+
+#ifdef CHECK
+  int x=4;
+  int bitpo=2;
+  bitpo = (x >> bitpo) & 1;
+  printf("the 2 bit is %d\n",bitpo);
+#endif
+  
+return 0;
+} 
+
+
+
+
+
+
