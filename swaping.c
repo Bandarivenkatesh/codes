03d344d8bd7f270441ac65b550aa420bb5bc1bf9
venkatesh <mr.bandarivenkatesh@gmail.com>
2018-02-17 16:24:48 +0530

    swap file in branch_one

---
+++ swaping.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main()
+    {
+	    unsigned char data=0x0A;
+
+	    // swaping 1st bit to 2nd bit (bit counting 7-0).
+	    // binary of 0x0A is : 0000 1010
+
+	    data^=(1<<1);
+	    data^=(1<<2);
+
+	    // data will be : 0000 1100 (0x0C)
+	    printf("\ndata after swap bits : %02X",data);
+
+	    return 0;
+    }
